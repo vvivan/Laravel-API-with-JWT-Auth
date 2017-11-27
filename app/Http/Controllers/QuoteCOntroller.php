b7264185931d2d5d737bd0c092c130665426fbeb
Ivan Videnovic <ivan.videnovic@quantox.com>
2017-11-27 13:19:00 +0100

    Post Route Works - Adding new quote

---
+++ app/Http/Controllers/QuoteCOntroller.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteCOntroller extends Controller {
+
+    public function postQuote(Request $request)
+    {
+        $quote = new Quote();
+        $quote->content = $request->input('content');
+        $quote->save();
+
+        return response()->json(['quote' => $quote], 201);
+    }
+
+    public function getQuotes()
+    {
+        $quotes = Quote::all();
+        $response = [
+            'quotes' => $quotes
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function putQuote(Request $request, $id)
+    {
+        $quote = Quote::find($id);
+        if (!quote) {
+            return response()->json(['message' => 'Document not found'], 404);
+        }
+        $quote->content = $request->input('content');
+        $quote->save();
+
+        return response()->json(['quote' => $quote], 200);
+    }
+
+    public function deleteQuote($id)
+    {
+        $quote = Quote::find($id);
+        $quote->delete();
+
+        return response()->json(['message' => 'Quote deleted'], 200);
+    }
+}
