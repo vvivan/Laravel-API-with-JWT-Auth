80fc88ae9388e7dcb6cff3a97310c22c7f2a7d49
Ivan Videnovic <ivan.videnovic@quantox.com>
2017-11-28 16:17:28 +0100

    Getting the Token on Signin Works

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
+
+class UserController extends Controller {
+    public function signup(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = new User([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+            ],201);
+    }
+
+    public function signin(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+        try {
+            if(!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'error' => 'Invalid Credentials'
+                ], 401);
+            }
+        } catch(JWTException $e) {
+            return response()->json([
+                'error' => 'Could not create token!'
+            ], 500);
+        }
+        return response()->json([
+            'token' => $token
+        ], 200);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
          * Package Service Providers...
          */
 
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
 
     ],
 
